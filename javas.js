fb14c91cb8e44c7cadfda845ea913bc07fd510db
ADITYA-KUMAR-THAKURAI <itsaditya09122003@gmail.com>
2025-07-07 20:12:09 +0530

    PORTFOLIO PROJECT

---
+++ javas.js
@@ -0,0 +1,8 @@
+function showProjectDetails(projectName) {
+  alert(`You clicked on ${projectName}!`);
+}
+
+function submitForm(event) {
+  event.preventDefault();
+  alert("Thanks for contacting me! I will get back to you soon.");
+}
